fdf57af788fa7b745fa4b2c299d4cef8c9d933e8
Jaspreet Singh <71896519+jaspreet7037@users.noreply.github.com>
2023-11-05 01:07:58 +0530

    navbar.tsx and authproiders.tsx files updated

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { getProviders, signIn } from 'next-auth/react';
-import react, { useState, useEffect } from 'react'
+import react, { useState, useEffect } from 'react';
+//import Button from "./Button";
 
 type Provider = {
   id: string,
@@ -9,18 +10,30 @@ type Provider = {
   type: string,
   signinUrl: string,
   callbackUrl: string,
-  signInUrlParams: Record<string, string> | null;
+  signInUrlParams: Record<string, string> | undefined;
 }
 
 type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
   const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      console.log(res);
+      //setProviders(res);
+    }
+    fetchProviders();
+  }, []);
+
   if(providers) {
     return (
       <div>
         {Object.values(providers).map
-        ((provider: Provider))}
+        ((provider: Provider, i) => (
+          <button key={i} title='Sign In' /*handleCLick={() => signIn(provider?.id)}*/>{provider.id}</button>
+        ))}
       </div>
     )
   }
