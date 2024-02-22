a09bf1d926fa1293202b338801266a6d2f2bb8cc
Jaspreet Singh <71896519+jaspreet7037@users.noreply.github.com>
2024-02-22 13:22:21 +0530

    Components folder updated with new Files

---
+++ app/create-project/page.tsx
@@ -0,0 +1,15 @@
+import Modal from "@/components/Modal"
+import ProjectForm from "@/components/ProjectForm"
+
+const CreateProject = () => {
+  return (
+    <Modal>
+     <h3 className="modal-head-text">
+        Create a new Project
+     </h3>
+     <ProjectForm />
+    </Modal>
+  )
+}
+
+export default CreateProject
